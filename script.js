80a1e7056ed6175ed08577bd95d91391c4466759
Radu Petrachioiu <radunicolae14@yahoo.com>
2024-04-07 19:57:19 +0300

    About section

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const stickySection = document.querySelector('.sticky');
-console.log(stickySection);
+let lastScrollTop = 0;
 
 
-window.addEventListener('scroll', (e)=>{
-        transform(stickySection);
+window.addEventListener('scroll', ()=>{
+    let navSelector = document.querySelector('nav');
+    transform(stickySection);
+
+    let currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        navSelector.style.height = '0';
+        navSelector.style.overflow = 'hidden';
+    } else{
+        navSelector.style.height = '70px';
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+
 })
 
 
@@ -12,6 +23,5 @@ function transform(section){
     const scrollSection = section.querySelector('.scroll-section');
     let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
     percentage = percentage < 0 ? 0 : percentage > 200 ? 200 : percentage;
-    console.log(percentage);
     scrollSection.style.transform = `translate3d(${-(percentage)}vw,0,0)`;
 }
