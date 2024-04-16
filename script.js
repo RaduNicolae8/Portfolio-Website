788e858f18199a9f3784096733626a4e215c1894
Radu Petrachioiu <radunicolae14@yahoo.com>
2024-04-17 02:47:38 +0300

    added nav observer

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let lastScrollTop = 0;
 
 window.addEventListener('scroll', ()=>{
     transform(stickySection);
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 })
 
 function transform(section){
@@ -14,3 +12,58 @@ function transform(section){
     percentage = percentage < 0 ? 0 : percentage > 200 ? 200 : percentage;
     scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+//script for when a section is into view, change .nav-line opacity to 1
+//use intersection observer
+// Select all navigation links
+const navLinks = document.querySelectorAll('.nav-line');
+
+// Options for the IntersectionObserver
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5 // Change as needed
+};
+
+// Callback function for intersection
+function intersectionCallback(entries, observer) {
+  entries.forEach(entry => {
+    // If section is in view
+    const navLine = document.querySelector(`.nav-menu-item:has([href='#${entry.target.id}'] ) .nav-line `); 
+    if (entry.isIntersecting) {
+      // Change opacity of associated nav-line to 1
+
+      if (navLine) {
+        navLine.style.opacity = 1;
+      }
+    } else {
+      // Reset opacity when section is not in view
+      if (navLine) {
+        navLine.style.opacity = 0;
+
+      }
+    }
+  });
+}
+
+// Create IntersectionObserver
+const observerWithHighThreshold = new IntersectionObserver(intersectionCallback, options);
+const observerWithSmallThreshold = new IntersectionObserver(intersectionCallback, { ...options, threshold: 0.2 });
+// Observe each section
+document.querySelectorAll('section').forEach(section => {
+  if (section.id === 'projects') {
+    observerWithSmallThreshold.observe(section);
+  } else {
+    observerWithHighThreshold.observe(section);
+  }
+});
